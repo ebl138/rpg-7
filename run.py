0eff972804ed5224f47bc7c57f6685f2f5bcc109
Ewan Black <ewanblack138@gmail.com>
2023-12-29 09:37:02 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+def main():
+    """
+    Run program
+    """
+    print("Running main")
+
+main()
