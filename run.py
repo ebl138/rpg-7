b939b5d4ee83d4fe2627579b838c697e12090d4b
Ewan Black <ewanblack138@gmail.com>
2023-12-29 09:52:16 +0000

    Implement get user_input function

--- run.py
+++ run.py
@@ -1,10 +1,21 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def get_user_input():
+    """
+    Get five comma-separated characters from user for password options.
+    Run while loop to repeat request for input until user input is valid;
+    input must be five comma-separated characters e.g. 18,Y,Y,5,N
+    """
+
+    user_input = input('Please enter your password options here: ')
+
+    print(f"In get_user_input() function and here is user input: {user_input}")
+
 def main():
     """
     Run program
     """
-    print("Running main")
+    print('Welcome to RPG-7 (Random Password Generator)!')
+    print()
+    user_input = get_user_input()
+    print()
 
 main()
